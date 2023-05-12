3b8376f666737132e84c518690b3e9a368eebfa0
falconDuty96 <falcon.duty96@gmail.com>
2023-05-12 13:21:44 +0300

    C+R OKey

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="lib/Uploader.css">
     <style>
-        
+
     </style>
 </head>
 
@@ -23,6 +23,38 @@
     <div id="fileReader" style="display: flex;flex-direction: row;flex-wrap: wrap;">
 
     </div>
+
+    <?php
+
+    try {
+        $conn = new PDO("mysql:host=localhost;dbname=imageuploader", "root", "");
+    } catch (PDOException $e) {
+        echo $e;
+        die();
+    }
+    $stmt = $conn->query("SELECT * FROM images WHERE 1");
+    $res = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $result = [];
+    if (count($res) > 0) {
+        $images = $res[0]->images_content;
+        $result = json_decode($images);
+    }
+
+    // var_dump($result) ;
+
+
+
+    ?>
+    <div id="fileReader" style="display: flex;flex-direction: row;flex-wrap: wrap;">
+        <?php for($i = 0; $i < count($result); $i++) : ?>
+            <div class="conteneur">
+            <div class="close-x" data-idelement="<?php echo $i ;?>" data-idparent="1">x</div>
+            <img data-name="<?php echo $result[$i] ;?>" style='margin-right: 10px;width: 250px; height: 250px; object-fit: cover;' src="http://localhost/image-uploader/<?php echo $result[$i] ;?>">
+        </div>
+        <?php endfor ; ?>    
+    
+    </div>
+    
 
 
     <script src="jquery.min.js"></script>
